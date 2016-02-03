54eb9ac23b563da28dcfce1c0b39915e8c447d40
max33nau <mtj29@nau.edu>
2016-02-02 16:38:44 -0800

    multiplayer works

--- src/multiplayer-app.js
+++ src/multiplayer-app.js
@@ -1,34 +1,40 @@
-
+"use strict";
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var db = require('./models/db');
-var blob = require('./models/blobs');
-var routes = require('./routes/index');
-var blobs = require('./routes/blobs');
 var express = require('express');
 var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var portNumber = 8080;
+var people = [];
+var rooms = {};
+var clients = {};
 
 var publicPath = path.join( __dirname, '../client/public/' );
 app.use( express.static(publicPath) );
 app.use( morgan('dev') );
+app.set('port', portNumber);
 
 app.get('/multiplayer', function(request,response) {
   response.redirect('multiplayer.html');
 });
 
-server.listen(3030, function(){
+server.listen(app.get('port'), function(){
   console.log('server is conncected')
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
   socket.on('move', function(data){
     socket.broadcast.emit('broad',data);
+  })
+
+  socket.on('hello', function(data){
+    people.push(data);
+    socket.emit('there', people);
   })
 
   socket.on('disconnect', function(){
