1a18ceb65f9b9601fe0b18f879fc93db75fe7ad7
andalex <and4alex@gmail.com>
2016-02-05 15:03:21 -0800

    fixed level-gen and to read in new .json format for levels.

--- src/models/level-gen.js
+++ src/models/level-gen.js
@@ -1,36 +1,29 @@
 'use strict';
 const db       = require('./db');
 const mongoose = require('mongoose');
-const levelData = require('./level-data');
-const pushesRocksLevels = require('./pushesRocksLevels.json');
-console.log(pushesRocksLevels);
+const levelData = require('./pushesRocksLevels.json');
+
 function createLevels() {
     var Schema = mongoose.Schema;
 
     var Level = new Schema({
-        difficulty: String,
-        level: Object
+      identifier: String,
+      difficulty: String,
+      data: Object,
     });
 
     var level = mongoose.model('level', Level);
-
     //drop collection to prevent duplicates before gen
     let rm = level.find().remove({});
     rm.exec();
 
-    for (let ii = 0; ii < levelData.easy.length; ii++) {
+    for (let ii = 0; ii < levelData.length; ii++) {
         var easyLevel = new level({
-            difficulty: 'easy',
-            level: levelData.easy[ii]
+            identifier: levelData[ii].identifier,
+            difficulty: levelData[ii].difficulty,
+            data: levelData[ii].data
         });
         easyLevel.save();
-    }
-    for (let ii = 0; ii < levelData.hard.length; ii++) {
-        var hardLevel = new level({
-            difficulty: 'hard',
-            level: levelData.hard[ii]
-        });
-        hardLevel.save();
     }
 }
 createLevels();
