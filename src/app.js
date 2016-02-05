71d1e403dea842bbca98efddbef69c0f3d1d0acf
andalex <and4alex@gmail.com>
2016-02-05 12:01:40 -0800

    modularize routes for login, play, and guestplay

--- src/app.js
+++ src/app.js
@@ -9,29 +9,30 @@ const session      = require('express-session');
 const MongoStore   = require('connect-mongo')(session);
 const Grant        = require('grant-express'),
       grant        = new Grant( require('./config/grantConfig') );
-      
+
 const multiplayer = require('./routes/multiplayer');
 const rooms = require('./routes/rooms');
 
 const data = require('./routes/data');
-
 
 const db = require('./models/db');
 const blob = require('./models/blobs');
 const routes = require('./routes/index');
 const blobs = require('./routes/blobs');
 const login = require( './routes/login' );
-
+const play = require( './routes/play' );
+const guestplay = require( './routes/guestplay' );
+const authenticated = require('./routes/authroute');
 function createApp() {
-	
+
 	var app = express();
 	var publicPath = path.join( __dirname, 'www/public/' );
-	
+
 	// view engine setup
 	app.set( 'views', path.join( __dirname, 'views' ) );
 	//app.set( 'view engine', 'hbs' );
 	app.set( 'view engine', 'jade' );
-	
+
 	app.use( favicon( path.join( publicPath, 'img/favicon.ico' ) ) );
 	app.use( morgan('dev') );
 
@@ -40,9 +41,9 @@ function createApp() {
 	// parse application/json
 	app.use( bodyParser.json() );
 	app.use( cookieParser() );
-	
+
 	app.use( express.static( publicPath ) );
-		
+
 	app.use('/', routes);
 //	app.use('/blobs', blobs);
 
@@ -55,8 +56,9 @@ function createApp() {
 		saveUninitialized: true
 	}));
 	app.use(grant);
-	app.use(login);	
-	
+  app.use(login);
+	app.use('/play', authenticated, play);
+  app.use('/guestplay', guestplay);
 	app.use("/data", data);
 
 	/* * * * * * * * * *
@@ -64,18 +66,18 @@ function createApp() {
 	 * * * * * * * * * */
 	app.use('/multiplayer', multiplayer());
 	app.use('/rooms', rooms());
-	
+
 	/* * * * * * * * * *
 	 * error handlers  *
 	 * * * * * * * * * */
-	 
+
 	// catch 404 and forward to error handler
 	app.use( (req, res, next) => {
 		var err = new Error('Not Found');
 		err.status = 404;
 		next(err);
 	});
-	
+
 	// development error handler
 	// will print stacktrace
 	if ( app.get('env') === 'development' ) {
@@ -87,7 +89,7 @@ function createApp() {
 			});
 		});
 	}
-	
+
 	// production error handler
 	// no stacktraces leaked to user
 	app.use( (err, req, res, next) => {
