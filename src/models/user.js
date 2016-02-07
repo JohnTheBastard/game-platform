56f6e3bf7660d301d305410d4d3032c193521088
John Hearn <jhearn@gmail.com>
2016-02-06 16:13:14 -0800

    unsaved changes

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+const GameSaveData = require('.gameSaveData');
 const User = new mongoose.Schema({
 	twitter: {
 		screen_name: String,
 		user_id: String,
 	},
-	game_data: [GameSaveData],
+	game_data: GameSaveData,
 	
 }, {
 	collection: 'user'
