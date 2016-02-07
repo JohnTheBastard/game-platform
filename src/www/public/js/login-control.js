2570488b09a52bc11bc3c3d850e38e62fb5714a4
andalex <and4alex@gmail.com>
2016-02-06 17:04:31 -0800

    failed push try 2

--- src/www/public/js/login-control.js
+++ src/www/public/js/login-control.js
@@ -1,5 +1,6 @@
 angular.module( 'myApp').controller('loginCtrl', function($scope, $window) {
     $scope.checkToken = function () {
+              //checking for a signed user token
                 var jwt = localStorage.getItem('token');
                 console.log(jwt);
                 if (jwt) {
