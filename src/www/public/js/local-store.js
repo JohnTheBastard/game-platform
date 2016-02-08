84836b1f27d54ef9a0fda5a7971e78ed77a45062
andalex <and4alex@gmail.com>
2016-02-08 09:29:21 -0800

    hotfix for localstore

--- src/www/public/js/local-store.js
+++ src/www/public/js/local-store.js
@@ -1,14 +1,11 @@
 var localStorage = (function() {
-
-    //get the token from uri
-    var jwt = window.location.search.split('=')[1];
-    var my = {};
-    //modify the uri remove the token query
-    //do this once /play refresh gets sorted
-    //history.replaceState({}, document.title, "/play");
-    my.getInput = function() {
-        my.username = $('#username').val();
-        localStorage.setItem("Name", JSON.stringify(my.username))
-    }
-    localStorage.setItem('token', jwt);
+  var level = localStorage.getItem('Level');
+  if(level){
+    console.log('localStore level');
+  } else {
+    localStorage.setItem("Level", JSON.stringify('0'));
+    // localStorage.setItem("Scores", JSON.stringify( this.levelScores ) );
+    localStorage.setItem("Difficulty", JSON.stringify('easy') );
+    localStorage.setItem("Initialized", JSON.stringify( 'true' ) );
+  }
 })();
